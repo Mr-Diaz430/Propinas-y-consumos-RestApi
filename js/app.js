f39997e9ce0ce39174610fc153f05d297bd42766
Mr-Diaz430 <Mr.diaz430@gmail.com>
2025-12-06 21:56:05 -0300

    test1UP

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 let cliente = {
     mesa: '',
     hora: '',
-    platillos: []
+    pedido: []
 }
 
 let categorias = {
@@ -96,13 +96,23 @@ function mostrarPlatillos(platillos) {
         inputCantidad.id = `producto-${platillo.id}`;
         inputCantidad.classList.add('form-control');
         
+        // si colocamos parentesis en la funcion, esta se ejecutara y no esperara a que ocurra el evento "onchange", si espera en caso de quitar los () pero ya no podremos enviar información.
+        /* Ejemplo
+        * se ejecuta: agregarPlatillo(platillo.id)
+        * espera: agregarPlatillo
+        * solucion (espera y puede enviar parametros): function(){agregarPlatillo(platillo.id)} -> funcion LINEAL
+        */
+        inputCantidad.onchange = function() {
+            const cantidad = parseInt(inputCantidad.value);
+            agregarPlatillo({...platillo, cantidad});
+        };
+
+
         const agregar = document.createElement('DIV');
         agregar.classList.add('col-md-2');
         agregar.appendChild(inputCantidad);
         
-        
-        console.log(inputCantidad);
-        
+            
         row.appendChild(nombre);
         row.appendChild(precio);
         row.appendChild(categoria);
@@ -110,4 +120,36 @@ function mostrarPlatillos(platillos) {
         contenido.appendChild(row);
         
     })
+}
+
+
+function agregarPlatillo(producto){
+
+    let { pedido } = cliente; 
+
+    // cantidad > 0
+    if (producto.cantidad > 0) {
+
+        if (pedido.some(articulo => articulo.id === producto.id)) {
+            // Si existe el articulo
+            const pedidoActualizado = pedido.map( articulo => {
+                if (articulo.id === producto.id) {
+                    articulo.cantidad = producto.cantidad;
+                }
+                return articulo;
+            });
+            cliente.pedido = [...pedidoActualizado];
+
+        }else{
+            // No existe el articulo
+            cliente.pedido = [...pedido, producto];
+        }
+        
+    }else{
+        console.log('No es mayor que 0');
+        const resultado = pedido.filter( articulo => articulo.id !== producto.id);
+        cliente.pedido = [...resultado];
+    }
+
+    console.log(cliente.pedido);
 }
