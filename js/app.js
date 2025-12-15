5be89a97d49a23ed5e170beb66cd30e3c7c9fafa
Mr-Diaz430 <Mr.diaz430@gmail.com>
2025-12-15 15:44:34 -0300

    total de productos

--- js/app.js
+++ js/app.js
@@ -197,12 +197,65 @@ function actualizarResumen() {
 
 
     // Iterar pedidos
+    const grupo = document.createElement('UL');
+    grupo.classList.add('list-group');
 
+    const { pedido } = cliente;
+    pedido.forEach( articulo => {
+        const { nombre, cantidad, precio, id } = articulo;
+        const lista = document.createElement('LI');
+        lista.classList.add('list-group-item');
+
+        const nombreEl = document.createElement('H4');
+        nombreEl.classList.add('my-4'); 
+        nombreEl.textContent = 'nombre';
+
+        // Cantidad del articulo
+        const cantidadEl = document.createElement('P');
+        cantidadEl.classList.add('fw-bold');
+        cantidadEl.textContent = 'Cantidad: ';
+
+        const cantidadValor = document.createElement('SPAN');
+        cantidadValor.classList.add('fw-normal');
+        cantidadValor.textContent = cantidad;
+
+        // Precio del articulo
+        const precioEl = document.createElement('P');
+        precioEl.classList.add('fw-bold');
+        precioEl.textContent = 'Precio: ';
+
+        const precioValor = document.createElement('SPAN');
+        precioValor.classList.add('fw-normal');
+        precioValor.textContent = `$${precio}`;
+
+        // Subtotal del articulo
+        const subTotalEl = document.createElement('P');
+        subTotalEl.classList.add('fw-bold');
+        subTotalEl.textContent = 'Subtotal: ';
+
+        const subTotalValor = document.createElement('SPAN');
+        subTotalValor.classList.add('fw-normal');
+        subTotalValor.textContent = precio * cantidad;
+
+        // Valores agregados a sus respectivos contenedores
+        cantidadEl.appendChild(cantidadValor);
+        precioEl.appendChild(precioValor);
+        subTotalEl.appendChild(subTotalValor);
+
+        // Valores agregados a la lista
+        lista.appendChild(nombreEl);
+        lista.appendChild(cantidadEl);
+        lista.appendChild(precioEl);
+        lista.appendChild(subTotalEl);
+
+        grupo.appendChild(lista);
+    });
 
     // Agregar contenido
     resumen.appendChild(mesa);
     resumen.appendChild(hora);
     resumen.appendChild(heading);
+    resumen.appendChild(grupo);
     
     contenido.appendChild(resumen);
 } 
